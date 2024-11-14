7fbf2f8a5fa03a5d2335c9ab50308083378960b2
G Baskar <136879909+Baskar4@users.noreply.github.com>
2024-11-14 17:40:54 +0530

    Add files via upload

---
+++ SpringBootRestApisMainProjectApplication.java
@@ -0,0 +1,13 @@
+package com.techpalle1.springbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApisMainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApisMainProjectApplication.class, args);
+	}
+
+}
